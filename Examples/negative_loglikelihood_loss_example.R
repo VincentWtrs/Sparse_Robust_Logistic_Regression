7efd13de44a027b54164b85a273e6f45d00a3c07
VincentWtrs <VincentWtrs@users.noreply.github.com>
2018-09-10 19:09:20 +0200

    changed loss definition to be normalized by sample size, added example in eta terms

--- Examples/negative_loglikelihood_loss_example.R
+++ Examples/negative_loglikelihood_loss_example.R
@@ -2,7 +2,7 @@
 
 # Defining simple logistic loss function
 logloss <- function(y, pi){
-  loss <- -(y * log(pi) + (1 - y) * log(1 - pi))
+  loss <- -(y * log(pi) + (1 - y) * log(1 - pi))/length(y)
 }
 
 # Creating a fine grid of fictional predictions pi within unit interval
@@ -44,3 +44,28 @@ legend(x = "topright",
        col = c("black", "red"),
        lty = 1)
 
+## IN TERMS OF LINEAR PREDICTOR
+eta_grid <- log(pi_grid/(1 - pi_grid))
+
+## Plotting
+# For 0-outcomes (BLACK)
+plot(x = eta_grid,
+     y = loss0,
+     type = "l",
+     lty = 1,
+     lwd = 1,
+     col = "black",
+     xlab = "Linear predictor value",
+     ylab = "Negative log-likelihood loss")
+# For 1-outcomes (RED)
+lines(x = eta_grid,
+      y = loss1,
+      lty = 1,
+      lwd = 1,
+      col = "red")
+# Legend
+legend(x = "topright",
+       legend = c("y = 0",
+                  "y = 1"),
+       col = c("black", "red"),
+       lty = 1)
